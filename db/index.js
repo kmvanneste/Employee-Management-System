ed2c21019ba264ba57bb4cf6a50ca7190cbc2662
kmvanneste <kmvanneste17@gmail.com>
2020-08-23 12:58:09 -0400

    created file structure and completed the database

---
+++ db/index.js
@@ -0,0 +1,6 @@
+//class DB
+//constructor to connection
+//find all managers
+//select statements
+
+//write all queries to database
---
+++ index.js
@@ -0,0 +1,3 @@
+//switch case with inquirer prompts
+
+//if they choose x option run this inquirer
