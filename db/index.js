2595609b8e3ab2d18f7f70edefd7e0975dc6906f
kmvanneste <kmvanneste17@gmail.com>
2020-08-25 20:01:27 -0400

    added role, employee, and dept functions

--- db/index.js
+++ db/index.js
@@ -3,12 +3,7 @@ const cTable = require("console.table");
 const connection = require("./connection.js");
 var mysql = require("mysql");
 
-//class DB
-//constructor to connection
-//find all managers
-//select statements
 runSearch();
-//write all queries to database
 function runSearch() {
   inquirer
     .prompt({
@@ -44,11 +39,11 @@ function runSearch() {
           break;
 
         case "Add Employee Role":
-          addEmployeeRole();
+          addRole();
           break;
 
         case "Add Employee Department":
-          addEmployeeDept();
+          addDept();
           break;
 
         case "Update Employee Role":
@@ -62,7 +57,7 @@ function viewDepts() {
   var query = "SELECT name FROM employees.department";
   connection.query(query, function (err, res) {
     for (var i = 0; i < res.length; i++) {
-      console.log(res[i].name);
+      console.table(res[i].name);
     }
     runSearch();
   });
@@ -72,7 +67,7 @@ function viewEmployees() {
   var query = "SELECT first_name, last_name FROM employees.employee";
   connection.query(query, function (err, res) {
     for (var i = 0; i < res.length; i++) {
-      console.log(`${res[i].first_name} ${res[i].last_name}`);
+      console.table(`${res[i].first_name} ${res[i].last_name}`);
     }
     runSearch();
   });
@@ -82,7 +77,7 @@ function viewRoles() {
   var query = "SELECT title FROM employees.role";
   connection.query(query, function (err, res) {
     for (var i = 0; i < res.length; i++) {
-      console.log(res[i].title);
+      console.table(res[i].title);
     }
     runSearch();
   });
@@ -108,11 +103,9 @@ function addEmployee() {
         },
         {
             name: "manID",
-            type: "list",
-            message: "What is your manager ID?", 
-            choices: [1, 2, 3]
+            type: "input",
+            message: "What is your manager ID?"
         }
-
     ]).then(function(answer) {
       var query = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
       connection.query(query, [answer.firstName, answer.lastName, answer.roleID, answer.manID], function(err, res) {
@@ -125,69 +118,55 @@ function addEmployee() {
     });
 }
 
-function addEmployeeRole() {
-  // var query = "SELECT first_name, last_name FROM employees.employee";
-  //     connection.query(query, function(err, res) {
-  //         for (var i = 0; i < res.length; i++) {
-  //             console.log(`${res[i].first_name} ${res[i].last_name}`);
-  //         }
-  //         runSearch();
-  //     });
+function addRole() {
+    inquirer
+    .prompt([
+        {
+            name: "title",
+            type: "input",
+            message: "What is the title of the new role?",
+        },
+        {
+            name: "salary",
+            type: "input",
+            message: "What is the salary?" 
+        },
+        {
+            name: "departmentID",
+            type: "input",
+            message: "What is the Department ID for this new role? Please select 1 for sales, 2 for engineering, 3 for accounting, 4 for law.",
+            choices: [1, 2, 3, 4] 
+        },
+    ]).then(function(answer) {
+      var query = "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)";
+      connection.query(query, [answer.title, answer.salary, answer.departmentID], function(err, res) {
+        if (err) throw (err);
+        for (var i = 0; i < res.length; i++) {
+          console.table(`New Role: ${res[i].title} ${res[i].salary} ${res[i].departmentID}`);
+        }
+        runSearch();
+      });
+    });
 }
 
-function addEmployeeDept() {
-  //     var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-  //     connection.query(query, function(err, res) {
-  //       for (var i = 0; i < res.length; i++) {
-  //         console.log(res[i].artist);
-  //       }
-  //       runSearch();
-  //     });
-  //   }
-  //   function addEmployeeRole() {
-  //     inquirer
-  //       .prompt([
-  //         {
-  //           name: "start",
-  //           type: "input",
-  //           message: "Enter starting position: ",
-  //           validate: function(value) {
-  //             if (isNaN(value) === false) {
-  //               return true;
-  //             }
-  //             return false;
-  //           }
-  //         },
-  //         {
-  //           name: "end",
-  //           type: "input",
-  //           message: "Enter ending position: ",
-  //           validate: function(value) {
-  //             if (isNaN(value) === false) {
-  //               return true;
-  //             }
-  //             return false;
-  //           }
-  //         }
-  //       ])
-  //       .then(function(answer) {
-  //         var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-  //         connection.query(query, [answer.start, answer.end], function(err, res) {
-  //           for (var i = 0; i < res.length; i++) {
-  //             console.log(
-  //               "Position: " +
-  //                 res[i].position +
-  //                 " || Song: " +
-  //                 res[i].song +
-  //                 " || Artist: " +
-  //                 res[i].artist +
-  //                 " || Year: " +
-  //                 res[i].year
-  //             );
-  //           }
-  //           runSearch();
-  //         });
-  //       });
+function addDept() {
+    inquirer
+    .prompt([
+        {
+            name: "department",
+            type: "input",
+            message: "What is the name of the new department?",
+        },
+    ]).then(function(answer) {
+      var query = "INSERT INTO department (name) VALUES (?)";
+      connection.query(query, [answer.deparment], function(err, res) {
+        if (err) throw (err);
+        for (var i = 0; i < res.length; i++) {
+          console.table(`New Department: ${res[i].department}`);
+        }
+        runSearch();
+      });
+    });
 }
 
 function updateEmployeeRole() {
--- db/seed.sql
+++ db/seed.sql
@@ -1,29 +1,35 @@
-USE employees;
+use employees;
 
 INSERT INTO department
     (name)
 VALUES
-    ("Sales"),
-    ("Engineering"),
-    ("HR"),
-    ("IT");
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ("Sales Person", 50000, 1),
-    ("Mechanical Engineer", 120000, 2),
-    ("Software Engineer", 100000, 2),
-    ("Junior Engineer", 85000, 2),
-    ("HR Drone" 80000, 3),
-    ("IT Hero", 70000, 4);
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+
+-- Change the names for the employees
 
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ("Erin", "Platvoet", 1, 1),
-    ("Manoli", "Kouzemath", 2, 2),
-    ("Hojin", "M", 2, NULL),
-    ("Thomas", "Challamet", 2, NULL),
-    ("Kyle", "Andie", 3, 3),
-    ("Ed", "Gibbons", 4, 4);
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
